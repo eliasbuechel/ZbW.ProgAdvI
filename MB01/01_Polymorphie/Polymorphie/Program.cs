214699e1fe87443bfb1878bb7c588a7dbcd5e670
symas <tk@symas.ch>
2021-10-19 16:12:58 +0200

    rename, add polymorphism

---
+++ MB01/01_Polymorphie/Polymorphie/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Polymorphie {
+    class Program {
+        static void Main(string[] args) {
+            // Test für new, abstract, virtual
+            Flugzeug flg = new Flugzeug();
+            flg.Starten();
+            Hubschrauber hubi = new Hubschrauber();
+            hubi.Starten();
+
+
+
+            // Test Polymorphie - Auswirkungen der drei Varianten
+            //Luftfahrzeug[] arr = new Luftfahrzeug[4];
+            //arr[0] = new Flugzeug();
+            //arr[1] = new Hubschrauber();
+            //arr[2] = new Hubschrauber();
+            //arr[3] = new Flugzeug();
+            //foreach (Luftfahrzeug temp in arr) {
+            //    temp.Starten();
+            //}
+
+            //Console.ReadLine();
+        }
+    }
+
+    public class Luftfahrzeug {
+        public string Hersteller { get; set; }
+        public int Baujahr { get; set; }
+
+        public void Starten() {
+            Console.WriteLine("Das Luftfahrzeug startet.");
+        }
+    }
+
+    public class Flugzeug : Luftfahrzeug {
+        public double Spannweite { get; set; }
+    }
+
+    public class Hubschrauber : Luftfahrzeug {
+        public double Rotor { get; set; }
+    }
+}
