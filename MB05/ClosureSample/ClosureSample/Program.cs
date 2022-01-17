135a45d71e3f29eb66c3d516d8c67c836931a382
Thomas Kehl <tk@symas.ch>
2022-01-17 22:22:02 +0100

    ClosureSample added

---
+++ MB05/ClosureSample/ClosureSample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace ClosureSample {
+
+    public delegate int Operation(int value);
+
+    class Program {
+        static void Main(string[] args) {
+            // Was gibt Main auf der Console aus und wieso?
+
+            var ret = GetOperation();
+            Console.WriteLine(ret.a(5));
+            Console.WriteLine(ret.b(5));
+
+
+            //var oper = GetOperation();
+            //Console.WriteLine(oper(5));
+
+            //var oper1 = GetOperation();
+            //Console.WriteLine(oper1(5));
+
+            //Console.WriteLine(oper(5));
+            //Console.WriteLine(oper(5));
+            //Console.WriteLine(oper(10));
+            //Console.WriteLine(oper1(8));
+        }
+
+
+        public static (Operation a, Operation b) GetOperation() {
+            int multiplier = 5;
+            //Operation operation = x => x * multiplier++;
+            //return operation;
+
+            Operation operationA = delegate (int x) {
+                Console.WriteLine("A wird ausgeführt");
+                return x * multiplier++;
+            };
+            Operation operationB = delegate (int x) {
+                Console.WriteLine("B wird ausgeführt");
+                return x * multiplier++;
+            };
+
+            multiplier = 10;
+            return (operationA, operationB);
+        }
+    }
+
+
+
+}
